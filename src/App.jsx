9d3d55a88f8d43bafc2f27fe0ae9f5bf99e5c1d9
Ronny <ronnyd.1994@gmail.com>
2024-07-25 20:18:28 -0500

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-
-import CardImg from './components/Card'
-import { Box } from '@mui/material'
 
 export default function App() {
   return (
-    <Box sx={{
-      padding:{
-        xs: '0' ,
-        sm:'2rem'}
-    
-    }}> 
-    <Navbar/>
-    <CardImg/>
-    </Box>
+    <div>App</div>
   )
 }
